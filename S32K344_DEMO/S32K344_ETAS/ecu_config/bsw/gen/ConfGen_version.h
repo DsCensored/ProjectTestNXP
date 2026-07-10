7a0d167eed8820e15acea4bf711b9f5685b2863d
Castor1ce <2308878690@qq.com>
2026-07-10 15:23:38 +0800

    [NEW]add etas project

---
+++ S32K344_DEMO/S32K344_ETAS/ecu_config/bsw/gen/ConfGen_version.h
@@ -0,0 +1,19 @@
+/* ********************************************************************************************************************
+ *
+ * COPYRIGHT RESERVED, 2013 ETAS GmbH. All rights reserved.
+ * The reproduction, distribution and utilization of this document as well as the communication of its contents to
+ * others without explicit authorization is prohibited. Offenders will be held liable for the payment of damages.
+ * All rights reserved in the event of the grant of a patent, utility model or design.
+ *
+ ******************************************************************************************************************* */
+
+#ifndef CONFGEN_VERSION_H
+#define CONFGEN_VERSION_H
+
+#define CONFGEN_VERSION "RTA-BSW ConfGen 12.1.0"
+#define CONFGEN_ISOLAR_AB "ISOLAR-A/B 12.1.2"
+#define CONFGEN_GENERATED_ON "2023-12-01T15:08:22.6985903+08:00[Asia/Shanghai]"
+
+// 
+
+#endif
